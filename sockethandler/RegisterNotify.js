b5d27179b0011a692fcfbf66ae18f8a1ab002cd3
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-11-20 17:17:29 +0530

    fix the model

--- sockethandler/RegisterNotify.js
+++ sockethandler/RegisterNotify.js
@@ -2,15 +2,19 @@ import RegistationNotify from "../Models/RegistationNotifyModel.js";
 
 const registrenotify = (socket) => {
   socket.on("register", async (data) => {
-    const { fname, email } = data;
+    const { fname, email , date, time } = data;
 
     try {
-      const message = ` ${fname}, User have successfully registered ${email}! `;
+      const message = ` ${fname}, User have successfully registered ${email} on ${date} at ${time}! `;
       const registrationData = new RegistationNotify({
         email,
         name: fname,
         message,
+        date,
+        time,
       });
+
+      console.log("registrationData", registrationData);
 
       await registrationData.save();
 
