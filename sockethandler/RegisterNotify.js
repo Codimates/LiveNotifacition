181ae92aff06603c7372754abc7c6ed431c76702
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-12-01 14:24:39 +0530

    fix changes

--- sockethandler/RegisterNotify.js
+++ sockethandler/RegisterNotify.js
@@ -2,7 +2,7 @@ import RegistationNotify from "../Models/RegistationNotifyModel.js";
 
 const registrenotify = (socket) => {
   socket.on("register", async (data) => {
-    const { fname, email , date, time } = data;
+    const { fname, email, date, time } = data;
 
     try {
       const message = ` ${fname}, User have successfully registered ${email} on ${date} at ${time}! `;
@@ -27,4 +27,15 @@ const registrenotify = (socket) => {
   });
 };
 
-export default registrenotify;
+const getnotifacition = (io) => {
+  io.on("Registernotifyget", async (socket) => {
+    try {
+      const notifications = await RegistationNotify.find();
+      socket.emit("getNotifications", { notifications });
+    } catch (error) {
+      socket.emit("getNotificationsError", { message: error.message });
+    }
+  });
+};
+
+export { registrenotify, getnotifacition };
